8a1b7a94d763ab07e0bb60502e69bd22456400a8
manpreets1ngh <smanpreetsingh506@gmail.com>
2024-06-18 00:52:47 +0100

    update pages for cart functionality

--- Store/State/CheckoutState.cs
+++ Store/State/CheckoutState.cs
@@ -12,7 +12,7 @@ namespace ApplicationToSellThings.BlazorUI.Store.State
         public string PaymentOption { get; }
         public IEnumerable<CardDetail> CardDetails { get; }
         public CardDetail? SelectedCard { get; }
-        public bool IsOrderReady { get; }
+        public bool IsOrderReady { get; set; }
         public Dictionary<string, int> Quantities { get; set; }
         public decimal TotalPrice { get; private set; }
         public bool IsLoading { get; private set; }
@@ -43,13 +43,9 @@ namespace ApplicationToSellThings.BlazorUI.Store.State
             ErrorMessage = errorMessage;
         }
 
-        public bool IsValidOrder()
+        public bool EvaluateOrderReadiness(AddressViewModel address, string paymentOption, List<ProductViewModel> products, CardDetail card = null)
         {
-            return SelectedAddress != null &&
-                   !string.IsNullOrEmpty(PaymentOption) &&
-                   SelectedProducts != null && SelectedProducts.Any() &&
-                   (PaymentOption != "card" || (PaymentOption == "card" && SelectedCard != null));
+            return address != null && !string.IsNullOrEmpty(paymentOption) && products.Any() && (paymentOption != "card" || (paymentOption == "card" && card != null));
         }
-
     }
 }
